c241fcd34b9f8599cb955ca332f3a9764db3910d
domchom <67873793+domchom@users.noreply.github.com>
2023-05-09 14:46:07 -0500

    Update waveanalysis.py
    Implement multiprocessing to to save time when saving figures.

--- waveanalysis.py
+++ waveanalysis.py
@@ -6,11 +6,50 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from tqdm import tqdm
+import multiprocessing
 import matplotlib.pyplot as plt
 from waveanalysismods.customgui import BaseGUI, RollingGUI
 from waveanalysismods.processor import TotalSignalProcessor, RollingSignalProcessor
 
 np.seterr(divide='ignore', invalid='ignore')
+
+def save_plot(plot, plot_name, plot_dir):
+    """
+    Saves a Matplotlib plot to a PNG file with the given name in the specified directory.
+
+    Parameters:
+    -----------
+    - plot (matplotlib.pyplot.plot): The plot to be saved.
+    - plot_name (str): The name to give to the saved plot.
+    - plot_dir (str): A string representing the path to the directory where the plot should be saved. If the directory
+                    doesn't exist, it will be created.
+    """
+    if not os.path.exists(plot_dir):
+        os.makedirs(plot_dir)
+    plot.savefig(f'{plot_dir}/{plot_name}.png')
+
+def save_plots(plots, plot_dir):
+    """
+    Saves a dictionary of Matplotlib plots to PNG files in the specified directory using multiprocessing.
+
+    Parameters:
+    -----------
+    - plots (dict): A dictionary of Matplotlib plots, where the keys are the names of the plots and the values
+                    are the actual plot objects.
+    - plot_dir (str): A string representing the path to the directory where the plots should be saved. If the directory
+                    doesn't exist, it will be created.
+    """
+    # Create a multiprocessing Pool with as many processes as there are CPUs
+    pool = multiprocessing.Pool()
+
+    # Iterate over the plot dictionary and add each plot and its name to a list of arguments to pass to the function
+    args_list = [(plot, plot_name, plot_dir) for plot_name, plot in plots.items()]
+
+    # Use the multiprocessing Pool to run the save_plot function in parallel for all the plots
+    pool.starmap(save_plot, args_list)
+
+    # Close the multiprocessing Pool to free up resources
+    pool.close()
 
 def main():
     '''** GUI Window and sanity checks'''
@@ -230,27 +269,15 @@ def main():
 
                 if plot_ind_peaks:        
                     ind_peak_plots = processor.plot_ind_peak_props()
-                    ind_peak_path = os.path.join(im_save_path, 'Individual_peak_plots')
-                    if not os.path.exists(ind_peak_path):
-                        os.makedirs(ind_peak_path)
-                    for plot_name, plot in ind_peak_plots.items():
-                        plot.savefig(f'{ind_peak_path}/{plot_name}.png')
+                    save_plots(plots=ind_peak_plots, plot_dir=os.path.join(im_save_path, 'Individual_peak_plots'))
                 if plot_ind_ACFs:
                     ind_acf_plots = processor.plot_ind_acfs()
-                    ind_acf_path = os.path.join(im_save_path, 'Individual_ACF_plots')
-                    if not os.path.exists(ind_acf_path):
-                        os.makedirs(ind_acf_path)
-                    for plot_name, plot in ind_acf_plots.items():
-                        plot.savefig(f'{ind_acf_path}/{plot_name}.png')
+                    save_plots(plots=ind_acf_plots, plot_dir=os.path.join(im_save_path, 'Individual_ACF_plots'))
                 if plot_ind_CCFs:
                     if processor.num_channels == 1:
                         log_params['Miscellaneous'] = f'CCF plots were not generated for {file_name} because the image only has one channel'
                     ind_ccf_plots = processor.plot_ind_ccfs()
-                    ind_ccf_path = os.path.join(im_save_path, 'Individual_CCF_plots')
-                    if not os.path.exists(ind_ccf_path):
-                        os.makedirs(ind_ccf_path)
-                    for plot_name, plot in ind_ccf_plots.items():
-                        plot.savefig(f'{ind_ccf_path}/{plot_name}.png')
+                    save_plots(plots=ind_ccf_plots, plot_dir=os.path.join(im_save_path, 'Individual_CCF_plots'))
 
 
                 # Summarize the data for current image as dataframe, and save as .csv
